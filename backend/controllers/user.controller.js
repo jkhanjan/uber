0df2417a557890d3e78f6198551e94daa9f0f48a
jkhanjan <jhakhanjan62@gmail.com>
2024-11-27 23:00:28 +0530

    login done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -19,3 +19,24 @@ module.exports.registerUser = async (req, res, next) => {
   const token = user.generateToken();
   res.status(201).json({ user, token });
 };
+
+module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(401).json({ message: "invalid email or password" });
+  }
+  const isMatch = await user.comparePassword(password);
+
+  if (!isMatch) {
+    return res.status(401).json({ message: "invalid email or password" });
+  }
+
+  const token = user.generateToken();
+  res.status(200).json({ user, token });
+};
